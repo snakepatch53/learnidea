3f59f98099bc8518fecbdf2cee3d831cbd9cb73b
snakepatch53 <snakepatch53@gmail.com>
2023-04-20 01:49:06 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   public/css.public/register.css
    	modified:   public/js.public/register.js
    	modified:   src/templates/public.pages/register.php

--- public/js.public/register.js
+++ public/js.public/register.js
@@ -1,11 +1,328 @@
+// declaracion de objetos del DOM
 const $form = document.getElementById("form-login");
 const $show_pass = $form.querySelectorAll(".show-pass");
+const input_containers_array = $form.querySelectorAll(".item");
+const $modal_success = document.getElementById("modal_success");
+const $general_message = document.getElementById("general_message");
 
+//conffeti en el modal
+document.addEventListener("DOMContentLoaded", function () {
+    var confettiSettings = { target: "canvas-confetti" };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+});
+
+// define function to show password
 $show_pass.forEach(($element) => {
-    const $button = $element.querySelector("#show-hide");
+    const $button = $element.querySelector(".show-hide");
     const $input = $element.querySelector(".password");
     $button.onclick = () => {
         $element.classList.toggle("show");
         $input.type = $input.type === "password" ? "text" : "password";
     };
 });
+
+// validacion de formulario al escribir
+input_containers_array.forEach(($input_container) => {
+    const $input = $input_container.querySelector("input");
+    if (!$input) return;
+    const input_type = $input.type;
+    if (input_type === "submit") return;
+    if (input_type === "checkbox") {
+        $input.onchange = (e) => {
+            const input_value = e.target.checked;
+            const input_name = e.target.name;
+            validate($input_container, input_value, input_name, input_type);
+        };
+        return;
+    }
+
+    $input.onkeyup = (e) => {
+        const input_value = e.target.value;
+        const input_name = e.target.name;
+        validate($input_container, input_value, input_name, input_type);
+    };
+});
+
+// validacion de formulario al enviar
+$form.onsubmit = (e) => {
+    e.preventDefault();
+    let isValid = true;
+    input_containers_array.forEach(($input_container) => {
+        const $input = $input_container.querySelector("input");
+        if (!$input) return; // si no existe el input, no se valida
+        const input_value = $input.value;
+        const input_name = $input.name;
+        const input_type = $input.type;
+        const _isValid = validate($input_container, input_value, input_name, input_type);
+        if (!_isValid) isValid = false;
+    });
+    if (isValid) {
+        // aqui se envia el formulario
+        $modal_success.classList.add("open");
+        const formData = new FormData($form);
+        fetch_query(formData, "users", "register").then((res) => {
+            if (!res.response) {
+                $general_message.classList.remove("info");
+                $general_message.classList.remove("success");
+                $general_message.classList.add("error");
+                $general_message.textContent = res.message;
+                return;
+            }
+            $general_message.classList.remove("info");
+            $general_message.classList.remove("error");
+            $general_message.classList.add("success");
+            $general_message.textContent = "";
+            let user_shortname = $form["user_names"].value.split(" ")[0] + " " + $form["user_surnames"].value.split(" ")[0];
+            $modal_success.classList.remove("load");
+            $modal_success.querySelector(".user_name").textContent = user_shortname;
+        });
+    }
+};
+
+// funcion de validacion de campo
+const validate = ($input_container, input_value, input_name, input_type) => {
+    const validation_function = validation[input_name];
+    if (!validation_function) return true;
+    const isValid = validation_function(input_value);
+    if (!isValid.valid) {
+        $input_container.classList.remove("info");
+        $input_container.classList.remove("success");
+        $input_container.classList.add("error");
+        $input_container.querySelector(".message").textContent = isValid.message;
+        return false;
+    }
+    $input_container.classList.remove("info");
+    $input_container.classList.remove("error");
+    $input_container.classList.add("success");
+    $input_container.querySelector(".message").textContent = "";
+    return true;
+};
+
+// objeto de funciones de validacion
+const validation = {
+    user_cedula: (value) => {
+        let result = {
+            valid: true,
+            message: "Numero de cedula valido",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "La cedula es obligatoria";
+            result.valid = false;
+            return result;
+        }
+
+        // permite una longitud exacta de 10 caracteres
+        if (value.length !== 10) {
+            result.message = "La cedula debe tener 10 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        // validamos si el numero de cedula es totalmente valido
+        if (!isCedula(value)) {
+            result.message = "Numero de cedula invalido";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_names: (value) => {
+        let result = {
+            valid: true,
+            message: "Nombres válidos",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor ingrese sus nombres";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite un minimo de 3 caracteres
+        const regex = /^[a-zA-ZÀ-ÿ\s]{3,40}$/g;
+        if (!regex.test(value)) {
+            result.message = "El nombre debe tener entre 3 y 40 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_surnames: (value) => {
+        let result = {
+            valid: true,
+            message: "Apellidos válidos",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor ingrese sus apellidos";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite un minimo de 3 caracteres
+        const regex = /^[a-zA-ZÀ-ÿ\s]{3,40}$/g;
+        if (!regex.test(value)) {
+            result.message = "El apellido debe tener entre 3 y 40 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_email: (value) => {
+        let result = {
+            valid: true,
+            message: "Email válido",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor ingrese su email";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex valida un email
+        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        if (!regex.test(value)) {
+            result.message = "El email no es válido";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_phone: (value) => {
+        let result = {
+            valid: true,
+            message: "Numero de celular válido",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor ingrese su numero de celular";
+            result.valid = false;
+            return result;
+        }
+
+        // permite una longitud exacta de 10 caracteres
+        if (value.length !== 10) {
+            result.message = "El numero de celular debe tener 10 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex valida un numero de celular ecuatoriano
+        const regex = /^(09)[0-9]{8}$/g;
+        if (!regex.test(value)) {
+            result.message = "El numero de celular no es válido";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_pass: (value) => {
+        let result = {
+            valid: true,
+            message: "Contraseña válida",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor ingrese su contraseña";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite un minimo de 8 caracteres y cualquier otra cosa estara bien
+        const regex = /^.{8,}$/g;
+        if (!regex.test(value)) {
+            result.message = "La contraseña debe tener al menos 8 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite un maximo de 20 caracteres y cualquier otra cosa estara bien
+        const regex2 = /^.{0,20}$/g;
+        if (!regex2.test(value)) {
+            result.message = "La contraseña debe tener maximo 20 caracteres";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite al menos una letra mayuscula y cualquier otra cosa estara bien
+        const regex3 = /^(?=.*[A-Z]).+$/g;
+        if (!regex3.test(value)) {
+            result.message = "La contraseña debe tener al menos una letra mayuscula";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite al menos un numero y cualquier otra cosa estara bien
+        const regex4 = /^(?=.*[0-9]).+$/g;
+        if (!regex4.test(value)) {
+            result.message = "La contraseña debe tener al menos un numero";
+            result.valid = false;
+            return result;
+        }
+
+        // este regex permite al menos un caracter especial y cualquier otra cosa estara bien
+        const regex5 = /^(?=.*[!@#$%^&*]).+$/g;
+        if (!regex5.test(value)) {
+            result.message = "La contraseña debe tener al menos un caracter especial";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_confirmpass: function (value) {
+        let result = {
+            valid: true,
+            message: "Contraseña válida",
+        };
+
+        // si el valor es vacio
+        if (value.length < 1) {
+            result.message = "Por favor confirme su contraseña";
+            result.valid = false;
+            return result;
+        }
+
+        // si el valor es diferente a la contraseña
+        if (value !== $form["user_pass"].value) {
+            result.message = "Las contraseñas no coinciden";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+    user_terminos: function (value) {
+        let result = {
+            valid: true,
+            message: "Terminos aceptados",
+        };
+
+        // si el valor es vacio
+        $check = $form["user_terminos"];
+        if (!$check.checked) {
+            result.message = "Debe aceptar los terminos y condiciones";
+            result.valid = false;
+            return result;
+        }
+
+        return result;
+    },
+};
+
+// mas funciones UI
+// function sho
--- src/templates/public.pages/register.php
+++ src/templates/public.pages/register.php
@@ -4,6 +4,7 @@
 <head>
     <title>Registro | Learnidea</title>
     <?php include('./src/templates/public.component/head.php') ?>
+    <!-- <link rel="stylesheet" href="<?= $DATA['http_domain'] ?>public/library.general/confetti.min.css"> -->
     <link rel="stylesheet" href="<?= $DATA['http_domain'] ?>public/css.public/register.css">
 </head>
 
@@ -24,86 +25,101 @@
                     <div class="item col-2">
                         <label for="email">Cédula</label>
                         <div class="input">
-                            <input type="text" name="cedula" placeholder="Número de cédula">
+                            <input type="text" name="user_cedula" placeholder="Número de cédula">
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="email">Nombres</label>
                         <div class="input">
-                            <input type="text" name="nombres" placeholder="Nombres">
+                            <input type="text" name="user_names" placeholder="Nombres">
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="email">Apellidos</label>
                         <div class="input">
-                            <input type="text" name="apellidos" placeholder="Apellidos">
+                            <input type="text" name="user_surnames" placeholder="Apellidos">
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item radio col-2">
                         <label for="email">Formación académica</label>
                         <div class="input">
-                            <input type="radio" name="formacion" id="formacion-basica" checked>
+                            <input value="1" type="radio" name="user_level" id="formacion-basica" checked>
                             <label for="formacion-basica">Básica</label>
-                            <input type="radio" name="formacion" id="formacion-secundaria">
+                            <input value="2" type="radio" name="user_level" id="formacion-secundaria">
                             <label for="formacion-secundaria">Secudaria</label>
-                            <input type="radio" name="formacion" id="formacion-superior">
+                            <input value="3" type="radio" name="user_level" id="formacion-superior">
                             <label for="formacion-superior">Superior</label>
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="email">Correo electrónico</label>
                         <div class="input">
-                            <input type="email" name="email" placeholder="Correo electrónico">
+                            <input type="email" name="user_email" placeholder="Correo electrónico">
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="email">Celular</label>
                         <div class="input">
-                            <input type="text" name="celular" placeholder="Número de celular">
+                            <input type="text" name="user_phone" placeholder="Número de celular">
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item radio col-2">
                         <label for="email">Sexo</label>
                         <div class="input">
-                            <input type="radio" name="sexo" id="sexo-masculino" checked>
+                            <input value="M" type="radio" name="user_sex" id="sexo-masculino" checked>
                             <label for="sexo-masculino">Masculino</label>
-                            <input type="radio" name="sexo" id="sexo-femenino">
+                            <input value="F" type="radio" name="user_sex" id="sexo-femenino">
                             <label for="sexo-femenino">Femenino</label>
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="password">Contraseña</label>
                         <div class="input show-pass">
-                            <input type="password" name="password" class="password" placeholder="Contraseña">
-                            <button id="show-hide">
+                            <input type="password" name="user_pass" class="password" placeholder="Contraseña">
+                            <button class="show-hide">
                                 <i class="fas fa-eye show"></i>
                                 <i class="fas fa-eye-slash hide"></i>
                             </button>
                         </div>
+                        <p class="message"></p>
                     </div>
 
                     <div class="item">
                         <label for="password">Confirma tu contraseña</label>
                         <div class="input show-pass">
-                            <input type="password" name="confirm_password" class="password" placeholder="Contraseña">
-                            <button id="show-hide">
+                            <input type="password" name="user_confirmpass" class="password" placeholder="Contraseña">
+                            <button class="show-hide">
                                 <i class="fas fa-eye show"></i>
                                 <i class="fas fa-eye-slash hide"></i>
                             </button>
                         </div>
+                        <p class="message"></p>
                     </div>
 
+
                     <div class="item col-2 terminos">
-                        <input type="checkbox" name="terminos" id="terminos">
-                        <label for="terminos">Acepto los <a href="<?= $DATA['http_domain'] ?>terminos-y-condiciones-de-uso/">términos y condiciones</a></label>
+                        <label class="input">
+                            <input type="checkbox" name="user_terminos" id="terminos">
+                            <label for="terminos">Acepto los <a href="<?= $DATA['http_domain'] ?>terminos-y-condiciones-de-uso/">términos y condiciones</a></label>
+                        </label>
+                        <p class="message"></p>
                     </div>
+
+                    <div class="item col-2 message success" id="general_message"></div><!-- clases info, error, success -->
 
                     <div class="item col-2 submit">
                         <div class="input">
@@ -123,11 +139,33 @@
             </form>
         </div>
 
+        <div class="modal_success load" id="modal_success">
+            <canvas id="canvas-confetti"></canvas>
+            <div class="container">
+                <div class="load">
+                    <i class="fas fa-spinner fa-spin"></i>
+                    <p>Registrando...</p>
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <i class="fas fa-check-circle"></i>
+                    </div>
+                    <div class="text">
+                        <h2>¡Registro exitoso!</h2>
+                        <p>Gracias <span class="user_name"></span> por registrarte en Learnidea.</p>
+                        <p>Revise su correo electrónico para activar tu cuenta.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </main>
 </body>
 
 <foot>
     <?php include('./src/templates/public.component/foot.php') ?>
+    <script src="<?= $DATA['http_domain'] ?>public/library.general/confetti.min.js"></script>
+    <script src="<?= $DATA['http_domain'] ?>public/js.general/validacion.js"></script>
     <script src="<?= $DATA['http_domain'] ?>public/js.public/register.js"></script>
 </foot>
 
